f190aa7db9811627f23b5dd0f0f53a4a69792d09
Arnaud Dumarché <a.dumarche@innocean.local>
2021-02-02 15:21:29 +0100

    more things

--- src/script.js
+++ src/script.js
@@ -1,31 +1,31 @@
-document.getElementById('carousel-arrow-next').addEventListener('click',carouselSwipe,false);
-document.getElementById('carousel-arrow-prev').addEventListener('click',carouselSwipe,false);
+// document.getElementById('carousel-arrow-next').addEventListener('click',carouselSwipe,false);
+// document.getElementById('carousel-arrow-prev').addEventListener('click',carouselSwipe,false);
 
 
-function carouselSwipe() {
-    var currentImg = document.getElementsByClassName('carousel-img-displayed')[0].id.substring(9);
-	var newImg = parseInt(currentImg);
-	if (this.id == 'carousel-arrow-next') {
-        newImg++;
-        if (newImg >= document.getElementsByClassName('carousel-img').length) {
-            newImg = 0;
-        }
-	} else if (this.id == 'carousel-arrow-prev') {
-        newImg--;
-        if (newImg<0) {
-            newImg = document.getElementsByClassName('carousel-img').length-1;
-        }
-    }
-    document.getElementById('carousel-'+currentImg).className = 'carousel-img carousel-img-hidden';
-    var displayedCarousel = document.getElementById('carousel-'+newImg);
-	displayedCarousel.className = 'carousel-img carousel-img-hidden';
-	setTimeout(function() {
-		displayedCarousel.className = 'carousel-img carousel-img-displayed';
-	},20);
-	setTimeout(function() {
-		document.getElementById('carousel-'+currentImg).className = 'carousel-img carousel-img-noDisplay';
-	},520);
-}
+// function carouselSwipe() {
+//     var currentImg = document.getElementsByClassName('carousel-img-displayed')[0].id.substring(9);
+// 	var newImg = parseInt(currentImg);
+// 	if (this.id == 'carousel-arrow-next') {
+//         newImg++;
+//         if (newImg >= document.getElementsByClassName('carousel-img').length) {
+//             newImg = 0;
+//         }
+// 	} else if (this.id == 'carousel-arrow-prev') {
+//         newImg--;
+//         if (newImg<0) {
+//             newImg = document.getElementsByClassName('carousel-img').length-1;
+//         }
+//     }
+//     document.getElementById('carousel-'+currentImg).className = 'carousel-img carousel-img-hidden';
+//     var displayedCarousel = document.getElementById('carousel-'+newImg);
+// 	displayedCarousel.className = 'carousel-img carousel-img-hidden';
+// 	setTimeout(function() {
+// 		displayedCarousel.className = 'carousel-img carousel-img-displayed';
+// 	},20);
+// 	setTimeout(function() {
+// 		document.getElementById('carousel-'+currentImg).className = 'carousel-img carousel-img-noDisplay';
+// 	},520);
+// }
 
 document.querySelector("#button").onclick = function() {
   if (window.getComputedStyle(document.querySelector('#conditions')).display=='none'){
