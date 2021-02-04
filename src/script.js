fb2fb5ab33ffd3fc6645a9fbd49c2f7a6a5aded9
Arnaud Dumarché <a.dumarche@innocean.local>
2021-02-04 16:40:36 +0100

    d

--- src/script.js
+++ src/script.js
@@ -61,3 +61,9 @@ $(function () {
     $('.bs-callout-warning').toggleClass('hidden', ok);
   })
 });
+
+window.addEventListener('resize', () => {
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
