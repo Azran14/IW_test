5ea87323eb3e5e020b3437ad8c8f7a2916f4348a
Arnaud Dumarché <a.dumarche@innocean.local>
2021-03-01 16:34:47 +0100

    hfiz

--- src/script.js
+++ src/script.js
@@ -1,3 +1,82 @@
+document.querySelector("#button").onclick = function() {
+  if (window.getComputedStyle(document.querySelector('#conditions')).display=='none'){
+  document.querySelector("#conditions").style.display="block";
+  document.querySelector("#form").style.display="none";
+  document.querySelector("#warranty").style.display="none";
+  } else {
+  document.querySelector("#conditions").style.display="none";
+  document.querySelector("#form").style.display="flex";
+  document.querySelector("#warranty").style.display="flex";
+  }
+}
+
+document.querySelector("#button2").onclick = function() {
+  if (window.getComputedStyle(document.querySelector('#conditions')).display=='none'){
+  document.querySelector("#conditions").style.display="block";
+  document.querySelector("#form").style.display="none";
+  document.querySelector("#warranty").style.display="none";
+  } else {
+  document.querySelector("#conditions").style.display="none";
+  document.querySelector("#form").style.display="flex";
+  document.querySelector("#warranty").style.display="flex";
+  }
+}
+
+var $ = require('jquery');
+require('parsleyjs');
+
+$(document).ready(function() {
+  $("#form").on('submit', function(e) {
+     e.preventDefault();
+     console.log("toto");
+     $(".validation").addClass("visible");
+     $(".main").removeClass("visible").addClass("hidden");
+  });
+});
+
+$("#carousel-3").on("mouseover", function() {
+  $("#visible-text").addClass("aftertitle__black").removeClass("aftertitle");
+  $("#visible-title").addClass("title__black").removeClass("title");
+});
+
+$("#carousel-1, #carousel-2, #carousel-4").on("mouseover", function() {
+  $("#visible-text").addClass("aftertitle").removeClass("aftertitle__black");
+  $("#visible-title").addClass("title").removeClass("title__black");
+});
+
+// -----------------------------------
+
+
+// https://codepen.io/desandro/pen/dpPzab
+// https://github.com/metafizzy/flickity/issues/104
+// https://developer.mozilla.org/fr/docs/Learn/JavaScript/Client-side_web_APIs/Video_and_audio_APIs
+// HTMLMediaElement.duration
+// Evenement propagation
+
+// -----------------------------------
+
+// document.querySelector("#button3").onclick = function() {
+//   if (window.getComputedStyle(document.querySelector('#validation')).display=='none'){
+//   document.querySelector("#validation").style.display="grid",
+//   document.querySelector("#validation").style.transition="1s",
+//   document.querySelector("#main").style.display="none";
+//   } else {
+//   document.querySelector("#validation").style.display="none";
+//   }
+// }
+
+// $(function () {
+//   $('#form').parsley().on('field:validated', function() {
+//     var ok = $('.parsley-error').length === 0;
+//     $('.bs-callout-info').toggleClass('hidden', !ok);
+//     $('.bs-callout-warning').toggleClass('hidden', ok);
+//   })
+// });
+
+// let vh = window.innerHeight * 0.01;
+// // Then we set the value in the --vh custom property to the root of the document
+// document.documentElement.style.setProperty('--vh', `${vh}px`);
+
 // document.getElementById('carousel-arrow-next').addEventListener('click',carouselSwipe,false);
 // document.getElementById('carousel-arrow-prev').addEventListener('click',carouselSwipe,false);
 
@@ -27,64 +106,6 @@
 // 	},520);
 // }
 
-document.querySelector("#button").onclick = function() {
-  if (window.getComputedStyle(document.querySelector('#conditions')).display=='none'){
-  document.querySelector("#conditions").style.display="block";
-  document.querySelector("#form").style.display="none";
-  document.querySelector("#warranty").style.display="none";
-  } else {
-  document.querySelector("#conditions").style.display="none";
-  document.querySelector("#form").style.display="flex";
-  document.querySelector("#warranty").style.display="flex";
-  }
-}
-
-document.querySelector("#button2").onclick = function() {
-  if (window.getComputedStyle(document.querySelector('#conditions')).display=='none'){
-  document.querySelector("#conditions").style.display="block";
-  document.querySelector("#form").style.display="none";
-  document.querySelector("#warranty").style.display="none";
-  } else {
-  document.querySelector("#conditions").style.display="none";
-  document.querySelector("#form").style.display="flex";
-  document.querySelector("#warranty").style.display="flex";
-  }
-}
-
-// document.querySelector("#button3").onclick = function() {
-//   if (window.getComputedStyle(document.querySelector('#validation')).display=='none'){
-//   document.querySelector("#validation").style.display="grid",
-//   document.querySelector("#validation").style.transition="1s",
-//   document.querySelector("#main").style.display="none";
-//   } else {
-//   document.querySelector("#validation").style.display="none";
-//   }
-// }
-
-
-// $(function () {
-//   $('#form').parsley().on('field:validated', function() {
-//     var ok = $('.parsley-error').length === 0;
-//     $('.bs-callout-info').toggleClass('hidden', !ok);
-//     $('.bs-callout-warning').toggleClass('hidden', ok);
-//   })
-// });
-
-// let vh = window.innerHeight * 0.01;
-// // Then we set the value in the --vh custom property to the root of the document
-// document.documentElement.style.setProperty('--vh', `${vh}px`);
-var $ = require('jquery');
-require('parsleyjs');
-
-$(document).ready(function() {
-  $("#form").on('submit', function(e) {
-     e.preventDefault();
-     console.log("toto");
-     $(".validation").addClass("visible");
-     $(".main").removeClass("visible").addClass("hidden");
-  });
-});
-
 // $("#carousel-4").on("mouseover", function() {
 //   $("#visible-text, #visible-offer, #visible-title").hide();
 // });
@@ -104,29 +125,6 @@ $(document).ready(function() {
 //   $("#visible-text").addClass("aftertitle").removeClass("aftertitle__black");
 //   $("#visible-title").addClass("title").removeClass("title__black");
 // };
-
-$("#carousel-3").on("mouseover", function() {
-  $("#visible-text").addClass("aftertitle__black").removeClass("aftertitle");
-  $("#visible-title").addClass("title__black").removeClass("title");
-});
-
-$("#carousel-1, #carousel-2, #carousel-4").on("mouseover", function() {
-  $("#visible-text").addClass("aftertitle").removeClass("aftertitle__black");
-  $("#visible-title").addClass("title").removeClass("title__black");
-});
-
-
-// -----------------------------------
-
-
-// https://codepen.io/desandro/pen/dpPzab
-// https://github.com/metafizzy/flickity/issues/104
-// https://developer.mozilla.org/fr/docs/Learn/JavaScript/Client-side_web_APIs/Video_and_audio_APIs
-// HTMLMediaElement.duration
-// Evenement propagation
-
-// -----------------------------------
-
 
 // if ($('#start').hasClass("is-selected")) {
 //   $(".main").removeClass("visible").addClass("hidden");
